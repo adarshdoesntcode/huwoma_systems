a7df9c75ade161ba5e30528cba5864960ecc5da5
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-11-25 20:03:19 +0545

    new error string

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -6,19 +6,22 @@ const Error = () => {
 
   console.error(error);
 
+  const errorString = error ? (
+    <>
+      {error.name}: {error.message}
+      <br />
+      Stack trace: {error.stack}
+    </>
+  ) : (
+    "An unknown error occurred."
+  );
+
   return (
     <div className="flex flex-1 items-center justify-center text-muted-foreground bg-inherit py-6 ">
       <div className="flex flex-col items-center gap-1 text-center">
         <h3 className="text-2xl font-semibold ">Oops! Something went wrong.</h3>
 
-        <div className="mt-4">
-          {error && (
-            <>
-              <p>{error.statusText || error.message}</p>
-            </>
-          )}
-          {JSON.stringify(error)}
-        </div>
+        <div className="mt-4">{errorString}</div>
       </div>
     </div>
   );
