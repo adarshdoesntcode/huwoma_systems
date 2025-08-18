c044cce4cf03e7bee30facc078dae629764ba8d6
Adarsh Das <adarsh.191605@ncit.edu.np>
2025-08-18 16:42:46 +0545

    feat: add form components for vehicle editing including input fields, multi-selects, and search functionality
    - Implemented InputArea, InputField, MultiInputField, SearchMultiSelect, and SearchSelect components for enhanced user input.
    - Created a centralized FormItems component to manage different input types dynamically.
    - Developed useBuyerInterestFormConfig and useFormConfig hooks to manage form state and validation for vehicle details.
    - Added validation logic for budget, year, mileage, and other vehicle specifications.
    - Integrated UI components for better user experience and responsiveness.

---
+++ src/features/garage/edit_vehicle/EditVehicleListing.jsx
@@ -0,0 +1,206 @@
+import { useParams } from "react-router-dom";
+
+import NavBackButton from "@/components/NavBackButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import Steps from "@/components/Steps";
+import PreviewVehicleListingForm from "./form/PreviewVehicleListingForm";
+import { cleanObject } from "@/lib/utils";
+import {
+  useEditVehicleListingsMutation,
+  useGetVehicleDetailsQuery,
+} from "../garageApiSlice";
+import { toast } from "@/hooks/use-toast";
+import { useNavigate } from "react-router-dom";
+import VehicleDetailsForm from "./form/VehicleDetailsForm";
+import VehicleImagesForm from "./form/VehicleImagesForm";
+
+function EditVehicleListing() {
+  const { id } = useParams();
+
+  const [selectedSeller, setSelectedSeller] = useState("");
+  const [selectedMake, setSelectedMake] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedTransmission, setSelectedTransmission] = useState("");
+  const [selectedFuelType, setSelectedFuelType] = useState("");
+  const [selectedDriveType, setSelectedDriveType] = useState("");
+  const [managedImages, setManagedImages] = useState([]);
+  const [finalImageUrls, setFinalImageUrls] = useState([]);
+
+  const [formStep, setFormStep] = useState(1);
+
+  const { data, isLoading, isFetching, isSuccess, isError, refetch, error } =
+    useGetVehicleDetailsQuery(id);
+
+  const [editVehicleListings] = useEditVehicleListingsMutation();
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    reset,
+    trigger,
+    setError,
+    clearErrors,
+    register,
+    getValues,
+    setFocus,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      const vehicle = data.data;
+      reset(vehicle);
+      setSelectedSeller(vehicle.seller);
+      setSelectedMake(vehicle.make);
+      setSelectedCategory(vehicle.category);
+      setSelectedTransmission(vehicle.transmission);
+      setSelectedFuelType(vehicle.fuelType);
+      setSelectedDriveType(vehicle.driveType);
+      setManagedImages(
+        vehicle.photos.map((image) => ({
+          urls: {
+            primaryUrl: image.primaryUrl,
+            fallbackUrl: image.fallbackUrl,
+          },
+          id: image._id,
+          type: "existing",
+        }))
+      );
+      setFinalImageUrls(vehicle.photos);
+    }
+  }, [isSuccess, data, reset]);
+
+  const onSubmit = async (data) => {
+    const vehiclePayload = cleanObject({
+      _id: data._id,
+      make: selectedMake,
+      category: selectedCategory,
+      transmission: selectedTransmission,
+      fuelType: selectedFuelType,
+      driveType: selectedDriveType,
+      model: data.model,
+      variant: data.variant,
+      year: data.year,
+      numberPlate: data.numberPlate,
+      mileage: data.mileage,
+      askingPrice: data.askingPrice,
+      engineCC: data.engineCC,
+      colour: data.colour,
+      description: data.description,
+      photos: finalImageUrls,
+    });
+
+    try {
+      const res = await editVehicleListings({
+        ...vehiclePayload,
+      });
+      if (res.error) {
+        throw new Error(res.error.data.message);
+      }
+      if (!res.error) {
+        toast({
+          title: "Vehicle Listed!",
+          description: "Successfully",
+          duration: 2000,
+        });
+        reset();
+        navigate(`/garage/vehicle/${id}`, { replace: true });
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong!!",
+        description: error.message,
+      });
+    }
+  };
+
+  let content;
+
+  content = (
+    <div className="grid items-start w-full max-w-4xl gap-4 mx-auto mb-64">
+      <div>
+        <NavBackButton buttonText={"Back"} navigateTo={-1} />
+      </div>
+      <Card className="p-0 bg-transparent border-none shadow-none">
+        <CardHeader className="p-0">
+          <div className="flex items-end justify-between px-1">
+            <div>
+              <CardTitle className="text-lg sm:text-xl">
+                Edit Vehicle Listing Form
+              </CardTitle>
+              <CardDescription className="text-xs sm:text-sm">
+                Edit the listed vehicle details
+              </CardDescription>
+            </div>
+            <Steps steps={3} current={formStep} />
+          </div>
+        </CardHeader>
+        <CardContent className="p-0 pt-4">
+          <form
+            id="edit-garage-vehicle-form"
+            onSubmit={handleSubmit(onSubmit)}
+            className="grid gap-6"
+          >
+            {formStep === 1 && (
+              <VehicleDetailsForm
+                register={register}
+                selectedMake={selectedMake}
+                setSelectedMake={setSelectedMake}
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+                selectedTransmission={selectedTransmission}
+                setSelectedTransmission={setSelectedTransmission}
+                selectedFuelType={selectedFuelType}
+                setSelectedFuelType={setSelectedFuelType}
+                selectedDriveType={selectedDriveType}
+                setSelectedDriveType={setSelectedDriveType}
+                setFormStep={setFormStep}
+                errors={errors}
+                reset={reset}
+                setError={setError}
+                trigger={trigger}
+                clearErrors={clearErrors}
+                setFocus={setFocus}
+              />
+            )}
+            {formStep === 2 && (
+              <VehicleImagesForm
+                setFormStep={setFormStep}
+                errors={errors}
+                managedImages={managedImages}
+                setManagedImages={setManagedImages}
+                setFinalImageUrls={setFinalImageUrls}
+                finalImageUrls={finalImageUrls}
+              />
+            )}
+            {formStep === 3 && (
+              <PreviewVehicleListingForm
+                setFormStep={setFormStep}
+                getValues={getValues}
+                selectedSeller={selectedSeller}
+                selectedMake={selectedMake}
+                selectedCategory={selectedCategory}
+                selectedTransmission={selectedTransmission}
+                selectedFuelType={selectedFuelType}
+                selectedDriveType={selectedDriveType}
+                finalImageUrls={finalImageUrls}
+              />
+            )}
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+
+  return content;
+}
+
+export default EditVehicleListing;
---
+++ src/features/garage/edit_vehicle/form/VehicleDetailsForm.jsx
@@ -0,0 +1,260 @@
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useGetVehicleConfigQuery } from "../../garageApiSlice";
+import Loader from "@/components/Loader";
+import ApiError from "@/components/error/ApiError";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import FormItems from "./form_items";
+import { useFormConfig } from "./useFormConfig";
+
+function VehicleDetailsForm({
+  register,
+  selectedMake,
+  setSelectedMake,
+  selectedCategory,
+  setSelectedCategory,
+  selectedTransmission,
+  setSelectedTransmission,
+  selectedFuelType,
+  setSelectedFuelType,
+  selectedDriveType,
+  setSelectedDriveType,
+  setFormStep,
+  errors,
+  setError,
+  trigger,
+  setFocus,
+  clearErrors,
+  reset,
+}) {
+  const { data, isLoading, isSuccess, isError, error, refetch } =
+    useGetVehicleConfigQuery();
+
+  const [makeData, setMakeData] = useState([]);
+  const [categoryData, setCategoryData] = useState([]);
+  const [transmissionData, setTransmissionData] = useState([]);
+  const [fuelTypeData, setFuelTypeData] = useState([]);
+  const [driveTypeData, setDriveTypeData] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setMakeData(data?.data?.vehicleMakers || []);
+      setCategoryData(data?.data?.vehicleCategories || []);
+      setTransmissionData(data?.data?.transmissionTypes || []);
+      setFuelTypeData(data?.data?.fuelTypes || []);
+      setDriveTypeData(data?.data?.driveTypes || []);
+    }
+  }, [data]);
+
+  const formConfig = useFormConfig(
+    register,
+    selectedMake,
+    setSelectedMake,
+    errors,
+    makeData,
+    clearErrors,
+    selectedCategory,
+    setSelectedCategory,
+    categoryData,
+    selectedTransmission,
+    setSelectedTransmission,
+    transmissionData,
+    selectedFuelType,
+    setSelectedFuelType,
+    fuelTypeData,
+    selectedDriveType,
+    setSelectedDriveType,
+    driveTypeData
+  );
+
+  const handleReset = () => {
+    setSelectedMake("");
+    setSelectedCategory("");
+    setSelectedTransmission("");
+    setSelectedFuelType("");
+    setSelectedDriveType("");
+    clearErrors([
+      "make",
+      "category",
+      "transmission",
+      "fuelType",
+      "driveType",
+      "model",
+      "year",
+      "numberPlate",
+      "mileage",
+      "askingPrice",
+    ]);
+    reset({
+      model: "",
+      year: "",
+      numberPlate: "",
+      mileage: "",
+      askingPrice: "",
+      engineCC: "",
+      colour: "",
+      description: "",
+    });
+  };
+
+  const handleNext = async () => {
+    const fieldsToValidate = [
+      { name: "make", value: selectedMake, message: "Please select a company" },
+      {
+        name: "category",
+        value: selectedCategory,
+        message: "Please select a category",
+      },
+      {
+        name: "transmission",
+        value: selectedTransmission,
+        message: "Please select a transmission type",
+      },
+      {
+        name: "fuelType",
+        value: selectedFuelType,
+        message: "Please select a fuel type",
+      },
+      {
+        name: "driveType",
+        value: selectedDriveType,
+        message: "Please select a drive type",
+      },
+    ];
+
+    let hasError = false;
+
+    fieldsToValidate.forEach((field) => {
+      if (!field.value) {
+        setError(field.name, { message: field.message });
+        hasError = true;
+      }
+    });
+
+    const isValid = await trigger([
+      "model",
+      "year",
+      "numberPlate",
+      "mileage",
+      "askingPrice",
+      "engineCC",
+      "colour",
+    ]);
+
+    if (!isValid) {
+      const firstErrorField = Object.keys(errors).reduce((field, key) => {
+        return errors[key] ? key : field;
+      }, "");
+
+      if (firstErrorField) {
+        setFocus(firstErrorField);
+      }
+    }
+
+    if (!hasError && isValid) {
+      const fieldNames = fieldsToValidate.map((field) => field.name);
+      clearErrors(fieldNames);
+      setFormStep(2);
+    }
+  };
+
+  const vehicleInfoConfig = formConfig.filter(
+    (config) => config.section === "vehicle-info"
+  );
+  const specificationConfig = formConfig.filter(
+    (config) => config.section === "specification"
+  );
+  const classificationConfig = formConfig.filter(
+    (config) => config.section === "classification"
+  );
+  const pricingConfig = formConfig.filter(
+    (config) => config.section === "pricing"
+  );
+  const descriptionConfig = formConfig.filter(
+    (config) => config.section === "description"
+  );
+
+  let content;
+  if (isLoading) {
+    content = (
+      <div className="flex items-center justify-center flex-1 h-full py-10 bg-white ">
+        <Loader />
+      </div>
+    );
+  } else if (isSuccess) {
+    content = (
+      <div className="duration-500 slide-in-from-right-5 animate-in">
+        <div className="grid gap-6 p-4 border shadow-sm rounded-xl sm:p-6 bg-background ">
+          <div>
+            <h3 className="text-lg font-semibold ">Vehicle Details Form</h3>
+            <p className="text-sm text-muted-foreground">
+              Complete the form below about the vehicle you are selling
+            </p>
+          </div>
+          <h3 className="pb-2 font-semibold border-b">Information</h3>
+          <div className="grid grid-cols-3 gap-4">
+            {vehicleInfoConfig.map((config, index) => (
+              <FormItems key={index} type={config.type} props={config} />
+            ))}
+          </div>
+
+          <h3 className="pb-2 font-semibold border-b">
+            Registration & Specification
+          </h3>
+          <div className="grid grid-cols-3 gap-4">
+            {specificationConfig.map((config, index) => (
+              <FormItems key={index} type={config.type} props={config} />
+            ))}
+          </div>
+
+          <h3 className="pb-2 font-semibold border-b">Classification</h3>
+          <div className="grid grid-cols-3 gap-4">
+            {classificationConfig.map((config, index) => (
+              <FormItems key={index} type={config.type} props={config} />
+            ))}
+          </div>
+          <h3 className="pb-2 font-semibold border-b">Pricing</h3>
+          <div className="grid grid-cols-3 gap-4">
+            {pricingConfig.map((config, index) => (
+              <FormItems key={index} type={config.type} props={config} />
+            ))}
+          </div>
+          <Separator />
+
+          <div className="grid grid-cols-3 gap-4">
+            {descriptionConfig.map((config, index) => (
+              <FormItems key={index} type={config.type} props={config} />
+            ))}
+          </div>
+          <Separator />
+
+          <div className="flex items-center justify-between gap-2">
+            <div className="flex items-center gap-2">
+              {/* <Button
+                variant="outline"
+                type="button"
+                onClick={() => setFormStep(0)}
+              >
+                <ChevronLeft className="w-4 h-4 mr-2" /> Back
+              </Button> */}
+              <div></div>
+              {/* <Button variant="ghost" type="button" onClick={handleReset}>
+                Reset
+              </Button> */}
+            </div>
+            <Button type="button" onClick={handleNext}>
+              Next <ChevronRight className="w-4 h-4 ml-2" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (isError) {
+    content = <ApiError error={error} refetch={refetch} />;
+  }
+
+  return content;
+}
+
+export default VehicleDetailsForm;
