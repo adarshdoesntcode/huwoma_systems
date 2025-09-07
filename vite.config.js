b547974200f86a6d432094a6ae97c94a847ae85a
Adarsh Das <adarsh.191605@ncit.edu.np>
2025-09-07 12:07:29 +0545

    pwa start

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.jpg"], // your static assets
+      manifest: {
+        name: "Huwoma Systems",
+        short_name: "Huwoma",
+        description: "Huwoma Systems PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
