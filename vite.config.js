63ad5606d83d4992a3c6bf7a628ee167d80eaff1
Adarsh Das <adarsh.191605@ncit.edu.np>
2025-09-07 12:28:57 +0545

    added icons

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,77 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["logo.jpg"], // your static assets
+      includeAssets: [
+        "icons/icon-48x48.png",
+        "icons/icon-72x72.png",
+        "icons/icon-96x96.png",
+        "icons/icon-128x128.png",
+        "icons/icon-144x144.png",
+        "icons/icon-152x152.png",
+        "icons/icon-192x192.png",
+        "icons/icon-256x256.png",
+        "icons/icon-384x384.png",
+        "icons/icon-512x512.png",
+      ],
       manifest: {
         name: "Huwoma Systems",
         short_name: "Huwoma",
-        description: "Huwoma Systems PWA",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        orientation: "portrait",
+        icons: [
+          {
+            src: "icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
         start_url: "/",
+        display: "standalone",
+        background_color: "#F8FAFC",
+        theme_color: "#ffffff",
       },
       workbox: {
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
